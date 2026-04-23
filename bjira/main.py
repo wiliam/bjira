c0bd922fe6a587ec72d69afd317f7e0c7788d530
wiliam <moltyaninov@gmail.com>
2026-04-23 22:41:38 +0800

    Add -v/--verbose to enable SDK debug logging

--- bjira/main.py
+++ bjira/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import logging
 from importlib import import_module
 from pkgutil import walk_packages
 
@@ -8,6 +9,8 @@ import bjira.operations
 
 def _parse_args():
     parser = argparse.ArgumentParser(description='jira helper')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='print HTTP calls and payloads to stderr')
     subparsers = parser.add_subparsers(help='sub-command help', required=True)
 
     for module_info in walk_packages(bjira.operations.__path__, bjira.operations.__name__ + '.'):
@@ -18,6 +21,9 @@ def _parse_args():
 
 def main():
     args = _parse_args()
+    if getattr(args, 'verbose', False):
+        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger('urllib3').setLevel(logging.WARNING)
     args.func(args)
 
 
